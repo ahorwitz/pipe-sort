637a66437e12df096ec455843cc3497f48e39622
Adam H <nisum@Admins-MacBook-Pro.local>
2017-08-18 11:52:35 -0700

    initial commit

---
+++ pipeSort.js
@@ -0,0 +1,4 @@
+export default (...fns) => (a, b) => {
+    const compare = (a, b, f = 0) => fns[f](a, b) ? fns[f](a, b) : fns[f](b, a) ? fns[f](b, a) : (f !== fns.length - 1) ? compare(a, b, f + 1) : 0;
+    return compare(a, b);
+};
