bb92480845d34d8b71761ccf6789c86c4884873a
Adam H <nisum@Admins-MacBook-Pro.local>
2017-08-23 10:11:30 -0700

    npm module

--- pipeSort.js
+++ pipeSort.js
@@ -1,4 +1,6 @@
-export default (...fns) => (a, b) => {
+module.exports = pipeC; 
+
+const pipeC = (...fns) => (a, b) => {
     const compare = (a, b, f = 0) => fns[f](a, b) ? fns[f](a, b) : fns[f](b, a) ? fns[f](b, a) : (f !== fns.length - 1) ? compare(a, b, f + 1) : 0;
     return compare(a, b);
 };
