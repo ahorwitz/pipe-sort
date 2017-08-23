bd3d11e47fb213884f788052009fea6d816add84
Adam H <nisum@Admins-MacBook-Pro.local>
2017-08-23 12:34:13 -0700

    add comp fn

--- pipeSort.js
+++ pipeSort.js
@@ -2,4 +2,12 @@ const pipeC = (...fns) => (a, b) => {
     const compare = (a, b, f = 0) => fns[f](a, b) ? fns[f](a, b) : fns[f](b, a) ? fns[f](b, a) : (f !== fns.length - 1) ? compare(a, b, f + 1) : 0;
     return compare(a, b);
 };
-module.exports = pipeC;
+
+const comparator = fn => (a, b) => fn(a, b) ? 1 : fn(b, a) ? -1 : 0;
+
+const pipeSort = {
+    comparator,
+    pipeC    
+};
+
+module.exports = pipeSort;
